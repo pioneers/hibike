d6ee25db52caa4b1727bf6ad3b5040881ec4e95b
jarzofjam <janny91355@gmail.com>
2015-12-05 15:54:41 -0800

    temp testing_basics.py

--- testing_basics.py
+++ testing_basics.py
@@ -1,10 +1,14 @@
 import serial
 import hibike_message as hm
 
-# create serial object (/dev//ttyACM0, 115200)
-#
-# start = time.time()
-# hm.send(object, hm.make_device_update(1, 20))
-# while hw.read(object) in (None, 1):
-#     pass
-# end = time.time()
+servo = serial.Serial(/dev//ttyACM0, 115200)
+time.sleep(1)
+
+print "Starting Latency Tests:"
+start = time.time()
+hm.send(servo, hm.make_device_update(1, 20))
+while hw.read(servo) in (None, 1):
+    pass
+end = time.time()
+
+print [end-start]
