8ea6d9971be60f431c72a71b826ffe44a60c61a5
jarzofjam <jannyzhang@berkeley.edu>
2015-12-05 15:45:13 -0800

    hard-coding latency tests because yay

---
+++ testing_basics.py
@@ -0,0 +1,10 @@
+import serial
+import hibike_message as hm
+
+# create serial object (/dev//ttyACM0, 115200)
+#
+# start = time.time()
+# hm.send(object, hm.make_device_update(1, 20))
+# while hw.read(object) in (None, 1):
+#     pass
+# end = time.time()
